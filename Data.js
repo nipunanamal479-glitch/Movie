086b3b0812cb7f501d18e8e9435eeebd21b8dd98
nipunanamal479-glitch <nipunanamal479@gmail.com>
2026-06-14 10:49:22 +0530

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,12 @@
+const movies = [
+    {
+        "title": "Avatar: Fire and Ash",
+        "image": "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar_The_Way_of_Water_poster.jpg",
+        "link": "https://google.com"
+    },
+    {
+        "title": "Dridam (2026)",
+        "image": "https://m.media-amazon.com/images/M/MV5BNzJjZGRjMDctMzE4YS00MTM1LTk2ZDYtMjcyMDJjYWIwMjJjXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_.jpg",
+        "link": "https://google.com"
+    }
+];
