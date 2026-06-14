b8dc2c0b7d07a002b67d07d37b50b138724b6801
nipunanamal479-glitch <nipunanamal479@gmail.com>
2026-06-14 15:33:13 +0530

    Update Data.js

--- Data.js
+++ Data.js
@@ -1,13 +1,13 @@
 const movies = [
     {
         "id": 1,
-        "title": "hutto",
+        "title": "යම රජ සිරි",
         "image": "pI7C8jr0beI-HD.jpg",
         "link": "https://google.com"
     },
     {
         "id": 2,
-        "title": "Dridam (2026)",
+        "title": "කාක්කන් (2026)",
         "image": "images/dridam.jpg",
         "link": "https://google.com"
     }
