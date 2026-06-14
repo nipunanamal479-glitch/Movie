0c72ee27efd2204f1322dac3931208a6f55b0fe8
nipunanamal479-glitch <nipunanamal479@gmail.com>
2026-06-14 21:56:02 +0530

    Update Data.js

--- Data.js
+++ Data.js
@@ -1,14 +1,5 @@
-const movies = [
-    {
-        "id": 1,
-        "title": "Yamaraja siri",
-        "image": "pI7C8jr0beI-HD.jpg",
-        "link": "https://google.com"
-    },
-    {
-        "id": 2,
-        "title": "කාක්කන් (2026)",
-        "image": "images/dridam.jpg",
-        "link": "https://google.com"
-    }
+window.movies = [
+    { id: 1, title: "Yamaraja siri", image: "pI7C8jr0beI-HD.jpg", description: "නින්ජා සටන් ඇතුළත් කතාව.", link: "#" },
+    { id: 2, title: "Dridam 2026", image: "https://via.placeholder.com/300", description: "තාත්වික චිත්‍රපටයක්.", link: "#" },
+    { id: 3, title: "One Piece", image: "https://via.placeholder.com/300", description: "මුහුදු කොල්ලකරුවන්ගේ වික්‍රමය.", link: "#" }
 ];
