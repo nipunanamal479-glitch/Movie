edfaefac3ed9799ec0a2c31d473308f29da94e95
nipunanamal479-glitch <nipunanamal479@gmail.com>
2026-06-17 18:17:18 +0530

    Update Data.js

--- Data.js
+++ Data.js
@@ -1,12 +1,5 @@
-// මේකෙන් තමයි UI එක පාලනය වෙන්නේ
-const config = {
-    title: "SL CINEMA HUB",
-    cardSize: "180px", // මෙතන 180px වගේ දාපන්, එතකොට පින්තූර ලොකුවට පේනවා
-    primaryColor: "#e50914"
-};
-
 const movies = [
-    { title: "Yamaraja Siri", img: "https://i.ibb.co/68v1r5K/movie1.jpg", desc: "නින්ජා සටන් ඇතුළත් කතාව.", link: "#" },
+    { title: "Yamaraja siri", img: "pI7C8jr0beI-HD.jpg", desc: " comedy.", link: "https://gofile.io/d/rnNPWx" },
     { title: "Dridam 2026", img: "https://i.ibb.co/68v1r5K/movie1.jpg", desc: "තාත්වික චිත්‍රපටයක්.", link: "#" },
-    { title: "One Piece", img: "https://i.ibb.co/68v1r5K/movie1.jpg", desc: "මුහුදු කොල්ලකරුවන්ගේ වික්‍රමය.", link: "#" }
+    { title: "Blades of Guardians", img: "https://i.ibb.co/68v1r5K/movie1.jpg", desc: "වික්‍රමාන්විත කතාවක්.", link: "#" }
 ];
