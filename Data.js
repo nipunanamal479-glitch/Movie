9fcb00d7dfc76523f5a302fc7045073bc1babe0e
nipunanamal479-glitch <nipunanamal479@gmail.com>
2026-06-17 16:59:04 +0530

    Update Data.js

--- Data.js
+++ Data.js
@@ -1,5 +1,3 @@
 window.movies = [
-    { id: 1, title: "Yamaraja siri", image: "pI7C8jr0beI-HD.jpg", description: "නින්ජා සටන් ඇතුළත් කතාව.", link: "#" },
-    { id: 2, title: "Dridam 2026", image: "https://via.placeholder.com/300", description: "තාත්වික චිත්‍රපටයක්.", link: "#" },
-    { id: 3, title: "One Piece", image: "https://via.placeholder.com/300", description: "මුහුදු කොල්ලකරුවන්ගේ වික්‍රමය.", link: "#" }
-];
+    { id: 1, title: "Yamaraja siri", image: "pI7C8jr0beI-HD.jpg", description: "comdy.", link: "https://gofile.io/d/rnNPWx" },
+    
