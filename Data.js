fcfa00df02230f3f1ad44d823b866f831b45b183
nipunanamal479-glitch <nipunanamal479@gmail.com>
2026-06-17 18:40:24 +0530

    Update Data.js

--- Data.js
+++ Data.js
@@ -1,6 +1,6 @@
 const movies = [
-    { title: "Yamaraja siri", img: "pI7C8jr0beI-HD.jpg", desc: " comedy.", link: "https://gofile.io/d/rnNPWx" },
+    { title: "Yamaraja siri", img: "pI7C8jr0beI-HD.jpg", desc: "comedy.", link: "https://gofile.io/d/rnNPWx" },
     { title: "Dridam 2026", img: "https://i.ibb.co/68v1r5K/movie1.jpg", desc: "තාත්වික චිත්‍රපටයක්.", link: "#" },
-    { title: "Blades of Guardians", img: "https://i.ibb.co/68v1r5K/movie1.jpg", desc: "වික්‍රමාන්විත කතාවක්.", link: "#" }
-    { title: "ponnya of tha pakaya", img: "pI7C8jr0beI-HD.jpg" desc: "huttak", link: "www.pornhub.com"}
+    { title: "Blades of Guardians", img: "https://i.ibb.co/68v1r5K/movie1.jpg", desc: "වික්‍රමාන්විත කතාවක්.", link: "#" },
+    { title: "Yamaraja siri", img: "pI7C8jr0beI-HD.jpg", desc: "comedy.", link: "https://gofile.io/d/rnNPWx" },
 ];
